431739f6910be93b942a8abf0205b9f5ab760606
lokywolf2295 <matiasesek1995@gmail.com>
2022-11-27 06:42:12 -0300

    I change the content for automated functions  IIFE

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,6 @@
+(() => {
 //guardamos la acción del boton formulario
 const btn = document.querySelector("[data-form-btn]");
-//console.log(btn);
 
 const createTask = (evento) => {
   evento.preventDefault(); //evita que se recargue la pagina borrando la información
@@ -11,19 +11,34 @@ const createTask = (evento) => {
   task.classList.add("card"); //agregamos una clase al task
   input.value = ""; //vaciamos el input
   //backticks
+
+  const taskContent = document.createElement("div"); //creo un elemento div
+  taskContent.appendChild(checkComplete()); //agregamos al div check
+  
+  const titleTask = document.createElement("span"); //creo un elemento span
+  titleTask.classList.add("task"); //agregamos la clase task al titleTask
+  titleTask.innerText = value; //agregamos al titleTask el valor del inpút
+  taskContent.appendChild(titleTask); //agregamos al contenido el titleTask
   const content = `
-    <div>
-        <i class="far fa-check-square icon"></i>
-        <span class="task">${value}</span>
-    </div>
     <i class="fas fa-trash-alt trashIcon icon"></i>
     `; //creamos el div y le pasamos el value para que lo guarde dentro
-  task.innerHTML = content; //agregamos al task el div con la info ingresada en el input
-
+  task.appendChild(taskContent); //agregamos al task el div con la info ingresada en el input
   list.appendChild(task); //agregamos a la lista el task
-
-  //console.log(content);
 };
 
-//listener => escucha el click del boton
 btn.addEventListener("click", createTask);
+
+const checkComplete = () => {
+  const i = document.createElement("i"); 
+  i.classList.add("far", "fa-check-square", "icon");
+  i.addEventListener("click", completeTask);
+  return i;
+};
+
+const completeTask = (event) =>{
+  const element = event.target;
+  element.classList.toggle("fas");
+  element.classList.toggle("completeIcon");
+  element.classList.toggle("far");
+}
+})();
